96c672a48b67913618e6da05d0c4078387220a32
KieceDonc <valentinverst.developer@gmail.com>
2020-11-19 19:50:42 +0100

    first commit

---
+++ dag.py
@@ -0,0 +1,103 @@
+class M(object):
+  def __init__(self):
+    "   "
+
+  def __add__( self, b):
+    return Plus(self, b)
+
+  def __mul__( self, b):
+    return Mult(self, b)
+
+  def __sub__(self, b):
+    return Plus(self, Opp(b))
+
+
+class Opp(M):
+  def __init__(self, a):
+    self.a=a
+
+  def eval(self,dico):
+    return (0-self.a.eval(dico))
+
+  def evalsymb(self,dico):
+    return (Opp(self.a.evalsymb(dico)))
+
+  def derivee(self,nomvar):
+    return Opp(self.a.derivee(nomvar))
+
+
+class Plus(M):
+  def __init__(self, a, b):
+    self.a=a
+    self.b=b
+ 
+  def eval(self,dico):
+    return(self.a.eval(dico)+self.b.eval(dico))
+
+  def evalsymb(self,dico):
+    return self.a.evalsymb(dico)+self.b.evalsymb(dico)
+
+  def derivee(self,nomvar):
+    return Plus(self.a.derivee(nomvar),self.b.derivee(nomvar))
+
+
+class Mult(M):
+  def __init__(self, a, b):
+    self.a=a
+    self.b=b
+
+  def eval(self,dico):
+    return(self.a.eval(dico)*self.b.eval(dico))
+
+  def evalsymb(self,dico):
+    return self.a.evalsymb(dico)*self.b.evalsymb(dico)
+
+  def derivee(self,nomvar):
+    return Mult(self.a*self.b.derivee(nomvar),self.b*self.a.derivee(nomvar))
+    
+
+class Nb(M):
+  def __init__(self, n):
+    self.nb=n
+
+  def eval(self,dico):
+    return self.nb
+
+  def evalsymb(self,dico):
+    return self.nb
+
+  def derivee(self,nomvar):
+    if(self.nb==nomvar):
+      return Nb(1)
+    else:
+      return (Nb(0))
+
+
+class Var(M):
+  def __init__(self, nom):
+    self.nom=nom
+
+  def eval(self,dico):
+    if(self.nom in dico):
+      return(dico[self.nom]) # pas sur
+    else :
+      return None # pas sur
+
+  def evalsymb(self,dico):
+    if(self.nom in dico):
+      return dico[self.nom]
+    else:
+      return self
+
+  def derivee(self,nomvar):
+    if(self.nom == nomvar):
+      return Nb(1)
+    else:
+      return Nb(0)
+
+
+x=Var("x")
+y=Var("y")
+z=Var("z")
+cercle = Plus(Plus(Mult(x,x),Mult(y,y)),Opp(Nb(-1)))
+print(cercle.eval( { 'x' : 1, 'y': -1}))
---
+++ polynome.py
@@ -0,0 +1,45 @@
+def somme(p,q):
+  n = len(p)
+  m = len(q)
+  r = max(n,m)
+  k = 0 
+  t = []
+  while k<r:
+    if(k<n and k<m):
+      t.insert(k,p[k] + q[k])
+    elif(k <n):
+      t.insert(k,p[k])
+    else :
+      t.insert(k,q[k])
+    k+=1
+  return t
+
+p = [6,4,3,17]
+q = [8,0,5]
+print(somme(p,q))
+
+def product(p,q):
+  n = len(p)-1
+  m = len(q)-1
+  k = 0 
+  t = [0 for i in range (n+m+1)]
+  for k in range(n+m+1):
+    ck = 0
+    for i in range(k+1) :
+      if (k-i<=m and i<=n):
+        ck+=p[i]*q[k-i]
+    t[k]=ck
+  return t
+
+p = [5,1]
+q = [3,2,1]
+print(product(p,q))
+
+def oppose(p):
+  t = []
+  for i in range(len(p)):
+    t.insert(i,p[i]*-1)
+  return t
+
+p = [1,2,-5,3,-9]
+print(oppose(p))
